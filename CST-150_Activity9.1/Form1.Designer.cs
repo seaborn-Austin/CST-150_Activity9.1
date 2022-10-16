d28e07d64e7871af2fd50c471e2be2f4448fba07
seaborn-Austin <64162552+seaborn-Austin@users.noreply.github.com>
2022-10-16 14:59:53 -0500

    Add project files.

---
+++ CST-150_Activity9.1/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace CST_150_Activity9._1
+{
+    partial class activity9Frm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(activity9Frm));
+            this.mmtoMeterLbl = new System.Windows.Forms.Label();
+            this.sumLBl = new System.Windows.Forms.Label();
+            this.sumRandomLbl = new System.Windows.Forms.Label();
+            this.threeIntDiv3Lbl = new System.Windows.Forms.Label();
+            this.fiveNumAveLbl = new System.Windows.Forms.Label();
+            this.fewestCharStringLbl = new System.Windows.Forms.Label();
+            this.longestDblLbl = new System.Windows.Forms.Label();
+            this.arrayOf50IntLbl = new System.Windows.Forms.Label();
+            this.boolCheckLBl = new System.Windows.Forms.Label();
+            this.productOfIntDoubleLBl = new System.Windows.Forms.Label();
+            this.twoDimensionalArrayLbl = new System.Windows.Forms.Label();
+            this.mmtoMeterTxt = new System.Windows.Forms.TextBox();
+            this.twoDimensionalArrayTxt = new System.Windows.Forms.TextBox();
+            this.productOfIntDoubleTxt = new System.Windows.Forms.TextBox();
+            this.boolCheckTxt = new System.Windows.Forms.TextBox();
+            this.arrayOf50IntTxt = new System.Windows.Forms.TextBox();
+            this.longestDblTxt = new System.Windows.Forms.TextBox();
+            this.fewestCharStringTxt = new System.Windows.Forms.TextBox();
+            this.threeIntDiv3Txt = new System.Windows.Forms.TextBox();
+            this.sumRandomTxt = new System.Windows.Forms.TextBox();
+            this.sumTxt = new System.Windows.Forms.TextBox();
+            this.fiveNumAveTxt = new System.Windows.Forms.TextBox();
+            this.processAllBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // mmtoMeterLbl
+            // 
+            this.mmtoMeterLbl.AutoSize = true;
+            this.mmtoMeterLbl.Location = new System.Drawing.Point(12, 35);
+            this.mmtoMeterLbl.Name = "mmtoMeterLbl";
+            this.mmtoMeterLbl.Size = new System.Drawing.Size(104, 13);
+            this.mmtoMeterLbl.TabIndex = 0;
+            this.mmtoMeterLbl.Text = "Millimeters to meters:";
+            // 
+            // sumLBl
+            // 
+            this.sumLBl.AutoSize = true;
+            this.sumLBl.Location = new System.Drawing.Point(12, 99);
+            this.sumLBl.Name = "sumLBl";
+            this.sumLBl.Size = new System.Drawing.Size(110, 13);
+            this.sumLBl.TabIndex = 1;
+            this.sumLBl.Text = "Sum of Two numbers:";
+            // 
+            // sumRandomLbl
+            // 
+            this.sumRandomLbl.AutoSize = true;
+            this.sumRandomLbl.Location = new System.Drawing.Point(12, 129);
+            this.sumRandomLbl.Name = "sumRandomLbl";
+            this.sumRandomLbl.Size = new System.Drawing.Size(282, 13);
+            this.sumRandomLbl.TabIndex = 2;
+            this.sumRandomLbl.Text = "Sum of two Random Numbers (Rerun for a different result):";
+            // 
+            // threeIntDiv3Lbl
+            // 
+            this.threeIntDiv3Lbl.AutoSize = true;
+            this.threeIntDiv3Lbl.Location = new System.Drawing.Point(12, 163);
+            this.threeIntDiv3Lbl.Name = "threeIntDiv3Lbl";
+            this.threeIntDiv3Lbl.Size = new System.Drawing.Size(248, 13);
+            this.threeIntDiv3Lbl.TabIndex = 3;
+            this.threeIntDiv3Lbl.Text = "This returns true if three numbers are divisible by 3: ";
+            // 
+            // fiveNumAveLbl
+            // 
+            this.fiveNumAveLbl.AutoSize = true;
+            this.fiveNumAveLbl.Location = new System.Drawing.Point(12, 66);
+            this.fiveNumAveLbl.Name = "fiveNumAveLbl";
+            this.fiveNumAveLbl.Size = new System.Drawing.Size(106, 13);
+            this.fiveNumAveLbl.TabIndex = 4;
+            this.fiveNumAveLbl.Text = "Sum of five numbers:";
+            // 
+            // fewestCharStringLbl
+            // 
+            this.fewestCharStringLbl.AutoSize = true;
+            this.fewestCharStringLbl.Location = new System.Drawing.Point(12, 196);
+            this.fewestCharStringLbl.Name = "fewestCharStringLbl";
+            this.fewestCharStringLbl.Size = new System.Drawing.Size(115, 13);
+            this.fewestCharStringLbl.TabIndex = 5;
+            this.fewestCharStringLbl.Text = "Returns shortest string:";
+            // 
+            // longestDblLbl
+            // 
+            this.longestDblLbl.AutoSize = true;
+            this.longestDblLbl.Location = new System.Drawing.Point(12, 227);
+            this.longestDblLbl.Name = "longestDblLbl";
+            this.longestDblLbl.Size = new System.Drawing.Size(138, 13);
+            this.longestDblLbl.TabIndex = 6;
+            this.longestDblLbl.Text = "Longest Double in an Array:";
+            // 
+            // arrayOf50IntLbl
+            // 
+            this.arrayOf50IntLbl.AutoSize = true;
+            this.arrayOf50IntLbl.Location = new System.Drawing.Point(12, 259);
+            this.arrayOf50IntLbl.Name = "arrayOf50IntLbl";
+            this.arrayOf50IntLbl.Size = new System.Drawing.Size(101, 13);
+            this.arrayOf50IntLbl.TabIndex = 7;
+            this.arrayOf50IntLbl.Text = "Array of 50 integers:";
+            // 
+            // boolCheckLBl
+            // 
+            this.boolCheckLBl.AutoSize = true;
+            this.boolCheckLBl.Location = new System.Drawing.Point(12, 288);
+            this.boolCheckLBl.Name = "boolCheckLBl";
+            this.boolCheckLBl.Size = new System.Drawing.Size(215, 13);
+            this.boolCheckLBl.TabIndex = 8;
+            this.boolCheckLBl.Text = "Returns true or false for two boolean values:";
+            // 
+            // productOfIntDoubleLBl
+            // 
+            this.productOfIntDoubleLBl.AutoSize = true;
+            this.productOfIntDoubleLBl.Location = new System.Drawing.Point(12, 319);
+            this.productOfIntDoubleLBl.Name = "productOfIntDoubleLBl";
+            this.productOfIntDoubleLBl.Size = new System.Drawing.Size(168, 13);
+            this.productOfIntDoubleLBl.TabIndex = 9;
+            this.productOfIntDoubleLBl.Text = "Returns product of int and double:";
+            // 
+            // twoDimensionalArrayLbl
+            // 
+            this.twoDimensionalArrayLbl.AutoSize = true;
+            this.twoDimensionalArrayLbl.Location = new System.Drawing.Point(12, 350);
+            this.twoDimensionalArrayLbl.Name = "twoDimensionalArrayLbl";
+            this.twoDimensionalArrayLbl.Size = new System.Drawing.Size(245, 13);
+            this.twoDimensionalArrayLbl.TabIndex = 10;
+            this.twoDimensionalArrayLbl.Text = "Returns average of two dimensional array integers:";
+            // 
+            // mmtoMeterTxt
+            // 
+            this.mmtoMeterTxt.Location = new System.Drawing.Point(122, 28);
+            this.mmtoMeterTxt.Name = "mmtoMeterTxt";
+            this.mmtoMeterTxt.Size = new System.Drawing.Size(100, 20);
+            this.mmtoMeterTxt.TabIndex = 11;
+            // 
+            // twoDimensionalArrayTxt
+            // 
+            this.twoDimensionalArrayTxt.Location = new System.Drawing.Point(263, 343);
+            this.twoDimensionalArrayTxt.Name = "twoDimensionalArrayTxt";
+            this.twoDimensionalArrayTxt.Size = new System.Drawing.Size(100, 20);
+            this.twoDimensionalArrayTxt.TabIndex = 12;
+            // 
+            // productOfIntDoubleTxt
+            // 
+            this.productOfIntDoubleTxt.Location = new System.Drawing.Point(186, 312);
+            this.productOfIntDoubleTxt.Name = "productOfIntDoubleTxt";
+            this.productOfIntDoubleTxt.Size = new System.Drawing.Size(100, 20);
+            this.productOfIntDoubleTxt.TabIndex = 13;
+            // 
+            // boolCheckTxt
+            // 
+            this.boolCheckTxt.Location = new System.Drawing.Point(233, 281);
+            this.boolCheckTxt.Name = "boolCheckTxt";
+            this.boolCheckTxt.Size = new System.Drawing.Size(100, 20);
+            this.boolCheckTxt.TabIndex = 14;
+            // 
+            // arrayOf50IntTxt
+            // 
+            this.arrayOf50IntTxt.Location = new System.Drawing.Point(119, 252);
+            this.arrayOf50IntTxt.Name = "arrayOf50IntTxt";
+            this.arrayOf50IntTxt.Size = new System.Drawing.Size(646, 20);
+            this.arrayOf50IntTxt.TabIndex = 15;
+            // 
+            // longestDblTxt
+            // 
+            this.longestDblTxt.Location = new System.Drawing.Point(156, 220);
+            this.longestDblTxt.Name = "longestDblTxt";
+            this.longestDblTxt.Size = new System.Drawing.Size(100, 20);
+            this.longestDblTxt.TabIndex = 16;
+            // 
+            // fewestCharStringTxt
+            // 
+            this.fewestCharStringTxt.Location = new System.Drawing.Point(130, 189);
+            this.fewestCharStringTxt.Name = "fewestCharStringTxt";
+            this.fewestCharStringTxt.Size = new System.Drawing.Size(100, 20);
+            this.fewestCharStringTxt.TabIndex = 17;
+            // 
+            // threeIntDiv3Txt
+            // 
+            this.threeIntDiv3Txt.Location = new System.Drawing.Point(266, 156);
+            this.threeIntDiv3Txt.Name = "threeIntDiv3Txt";
+            this.threeIntDiv3Txt.Size = new System.Drawing.Size(100, 20);
+            this.threeIntDiv3Txt.TabIndex = 18;
+            // 
+            // sumRandomTxt
+            // 
+            this.sumRandomTxt.Location = new System.Drawing.Point(300, 122);
+            this.sumRandomTxt.Name = "sumRandomTxt";
+            this.sumRandomTxt.Size = new System.Drawing.Size(100, 20);
+            this.sumRandomTxt.TabIndex = 19;
+            // 
+            // sumTxt
+            // 
+            this.sumTxt.Location = new System.Drawing.Point(128, 92);
+            this.sumTxt.Name = "sumTxt";
+            this.sumTxt.Size = new System.Drawing.Size(100, 20);
+            this.sumTxt.TabIndex = 20;
+            // 
+            // fiveNumAveTxt
+            // 
+            this.fiveNumAveTxt.Location = new System.Drawing.Point(124, 59);
+            this.fiveNumAveTxt.Name = "fiveNumAveTxt";
+            this.fiveNumAveTxt.Size = new System.Drawing.Size(100, 20);
+            this.fiveNumAveTxt.TabIndex = 21;
+            // 
+            // processAllBtn
+            // 
+            this.processAllBtn.Location = new System.Drawing.Point(300, 402);
+            this.processAllBtn.Name = "processAllBtn";
+            this.processAllBtn.Size = new System.Drawing.Size(75, 23);
+            this.processAllBtn.TabIndex = 22;
+            this.processAllBtn.Text = "Process all";
+            this.processAllBtn.UseVisualStyleBackColor = true;
+            this.processAllBtn.Click += new System.EventHandler(this.processAllBtn_Click);
+            // 
+            // activity9Frm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = global::CST_150_Activity9._1.Properties.Resources.Activity9_BckGr;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.processAllBtn);
+            this.Controls.Add(this.fiveNumAveTxt);
+            this.Controls.Add(this.sumTxt);
+            this.Controls.Add(this.sumRandomTxt);
+            this.Controls.Add(this.threeIntDiv3Txt);
+            this.Controls.Add(this.fewestCharStringTxt);
+            this.Controls.Add(this.longestDblTxt);
+            this.Controls.Add(this.arrayOf50IntTxt);
+            this.Controls.Add(this.boolCheckTxt);
+            this.Controls.Add(this.productOfIntDoubleTxt);
+            this.Controls.Add(this.twoDimensionalArrayTxt);
+            this.Controls.Add(this.mmtoMeterTxt);
+            this.Controls.Add(this.twoDimensionalArrayLbl);
+            this.Controls.Add(this.productOfIntDoubleLBl);
+            this.Controls.Add(this.boolCheckLBl);
+            this.Controls.Add(this.arrayOf50IntLbl);
+            this.Controls.Add(this.longestDblLbl);
+            this.Controls.Add(this.fewestCharStringLbl);
+            this.Controls.Add(this.fiveNumAveLbl);
+            this.Controls.Add(this.threeIntDiv3Lbl);
+            this.Controls.Add(this.sumRandomLbl);
+            this.Controls.Add(this.sumLBl);
+            this.Controls.Add(this.mmtoMeterLbl);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "activity9Frm";
+            this.Text = "Activity 9";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label mmtoMeterLbl;
+        private System.Windows.Forms.Label sumLBl;
+        private System.Windows.Forms.Label sumRandomLbl;
+        private System.Windows.Forms.Label threeIntDiv3Lbl;
+        private System.Windows.Forms.Label fiveNumAveLbl;
+        private System.Windows.Forms.Label fewestCharStringLbl;
+        private System.Windows.Forms.Label longestDblLbl;
+        private System.Windows.Forms.Label arrayOf50IntLbl;
+        private System.Windows.Forms.Label boolCheckLBl;
+        private System.Windows.Forms.Label productOfIntDoubleLBl;
+        private System.Windows.Forms.Label twoDimensionalArrayLbl;
+        private System.Windows.Forms.TextBox mmtoMeterTxt;
+        private System.Windows.Forms.TextBox twoDimensionalArrayTxt;
+        private System.Windows.Forms.TextBox productOfIntDoubleTxt;
+        private System.Windows.Forms.TextBox boolCheckTxt;
+        private System.Windows.Forms.TextBox arrayOf50IntTxt;
+        private System.Windows.Forms.TextBox longestDblTxt;
+        private System.Windows.Forms.TextBox fewestCharStringTxt;
+        private System.Windows.Forms.TextBox threeIntDiv3Txt;
+        private System.Windows.Forms.TextBox sumRandomTxt;
+        private System.Windows.Forms.TextBox sumTxt;
+        private System.Windows.Forms.TextBox fiveNumAveTxt;
+        private System.Windows.Forms.Button processAllBtn;
+    }
+}
+
---
+++ CST-150_Activity9.1/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CST_150_Activity9._1
+{
+    public partial class activity9Frm : Form
+    {
+        public activity9Frm()
+        {
+            InitializeComponent();
+        }
+
+        private void processAllBtn_Click(object sender, EventArgs e)
+        {
+           mmtoMeterTxt.Text = millimeterToMeter(1000).ToString();
+
+           fiveNumAveTxt.Text = fiveDoubles(1.0, 2.9, 3.8, 4.7, 5.6).ToString();
+
+           sumTxt.Text = sum(4.2, 4.8).ToString();
+
+           sumRandomTxt.Text = sumRandom().ToString();
+
+           threeIntDiv3Txt.Text = threeIntDiv(3, 6, 9).ToString();
+
+           fewestCharStringTxt.Text = shortestString("Hi", "World");
+
+            //Making the double array to find the longest double
+           double[] arr = {100.01, 2.2, 3.3, 4.4, 5.5, 6, 7.7 };
+           longestDblTxt.Text = doubleArray(arr).ToString();
+
+           arrayOf50IntTxt.Text = arrayOf50int().ToString();
+
+           boolCheckTxt.Text = checkBool(true, true).ToString();
+
+           productOfIntDoubleTxt.Text = intDblProduct(10, 5.123).ToString();
+
+           //Make the two dimensional array
+            int[][] a = new int[2][];
+            a[0] = new int[5] { 1, 2, 3, 4, 5 };
+            a[1] = new int[2] { 9, 8 };
+            twoDimensionalArrayTxt.Text = twoDimensionalArray(a).ToString();
+
+
+        }
+
+        public int millimeterToMeter(int numMillimeters)
+        {
+            //Returns the formula, millimeters / 1000
+            return numMillimeters / 1000;
+        }
+
+        public double fiveDoubles(double a, double b, double c, double d, double e)
+        {
+            //Adds all possible numbers up and divides them by 5 for the average
+            return (a + b + c + d + e) / 5.0;
+        }
+
+        public double sum(double a, double b)
+        {
+            //Adds two integers up for the sum
+            return a + b;
+        }
+        public int sumRandom()
+        {
+            //Declaring two random numbers
+            Random a = new Random();
+            Random b = new Random();
+
+            //Declaring two integers and assigning a random number to them
+            int c = a.Next();
+            int d = b.Next();
+
+            //Returning the sum of the two numbers
+            return c + d;
+           
+        }
+
+        public bool threeIntDiv(int a, int b, int c)
+        {
+            //Using modulo to determine if the numbers mod 3 are equal to 0
+            if(a % 3 == 0 && b % 3 == 0 && c % 3 == 0)
+            {
+                //Return true if they equal zero
+                return true;
+            }
+            //Return false if they equal anything other than zero
+            else { return false;  }
+        }
+
+        public string shortestString(string a, string b)
+        {
+            //Check if 'a' string length is greater than 'b' string length
+            if(a.Length > b.Length)
+            {
+                //Return 'b' if 'a' is greater
+                return b;
+            } 
+            //Otherwise return false
+            else { return a; }
+
+        }
+
+        public double doubleArray(double[] a)
+        {
+            //Sort the array, inspired by a java assignment on chegg
+            Array.Sort(a);
+
+
+            //Returning the Array's element - 1
+            return a[a.Length-1];
+        }
+
+        public int arrayOf50int()
+        {
+            int[] a = new int[50];
+            Random r = new Random();
+
+
+            int[] c = new int[50];
+            for(int i = 0; i < a.Length; i++)
+            {
+                a[i] = i;
+                Console.WriteLine(a[i] + ", ");
+            }
+            return a[49];
+
+        }
+
+       
+        public static bool checkBool(bool a, bool b)
+        {
+            //if value a and b are the same, return true, else return false
+            if(a == b)
+            {
+                return true;
+            }
+            else { return false;  }
+        }
+
+        public double intDblProduct(int a, double b)
+        {
+            //Return the product of the int and double
+            return a * b;
+        }
+
+        public int twoDimensionalArray(int[][] a)
+        {
+            int ave = 0;
+            int total = 0;
+            //First for loop selects the row
+            for (int i = 0; i < a.Length; i++)
+            {
+                //Second for loop goes through the contents of the array indexes
+                for(int j = 0; j < a[i].Length; j++)
+                {
+                    //The numbers at row i and column j is cycled through and the added value is added to total
+                    total += a[i][j];
+                    //Ave (average) is incremented by 1 each iteration
+                    ave++;
+                }
+            }
+            //return the total numbers 
+            return total / ave;
+        }
+    }
+}
